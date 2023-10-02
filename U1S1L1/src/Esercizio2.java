55957625f7b0e7fb35ee6fcff4c13e21e033cbbc
davide bragagnolo <davidebraga.99@gmail.com>
2023-10-02 16:54:30 +0200

    add ex 2

--- U1S1L1/src/Esercizio2.java
+++ U1S1L1/src/Esercizio2.java
@@ -7,5 +7,12 @@ public class Esercizio2 {
         String string1 = input.nextLine();
         System.out.println("Inserisci stringa 2: ");
         String string2 = input.nextLine();
+        System.out.println("Inserisci stringa 3: ");
+        String string3 = input.nextLine();
+        String totalStrings = string1.concat(string2).concat(string3);
+        String totalStringsReverse = string3.concat(string2).concat(string1);
+        System.out.println(totalStrings);
+        System.out.println(totalStringsReverse);
+        input.close();
     }
 }
